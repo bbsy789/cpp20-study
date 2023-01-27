5f757e1e988dfd607bab233bab765f26dd21a414
wwj <bbsy789@126.com>
2023-01-27 13:31:13 +0000

    bugs pair!

--- 1.4.5 模板类型推导CTAD/main.cpp
+++ 1.4.5 模板类型推导CTAD/main.cpp
@@ -1,7 +1,26 @@
 #include<cmath>
 #include<vector>
 
+//C++17新特性 模板类型推导
+//Class Template Argument Deduction，缩写为CTAD
+/*
+    进一步减少冗余代码的重要特性。当我们使用标准库里提供的模板函数时，
+    或许不知道它就是个模板函数，但使用起来就和普通函数一样自然。
+*/
 
+int foo = std::max(1,2); // #1,省略模板参数的指明,因为两个参数类型一致。
+double bar = std::max<double>(1, 2.9); // #2,指明模板参数，因为两个参数类型不一致，编译器不知道该使用哪一个类型。
+/*
+    然而在我们使用模板类时，都必须指明模板类的模板参数后才能使用。
+*/
+std::vector<int> foo2{1,2,3,4};
+std::pair<int, double> bar2{1, 2.0};
+/*
+    初始化一个序对类型时，我们明显可以通过构造传递的两个值的类型得到pair<int，double>,
+    但是在C++17之前，尽管编译器知道这些类型，仍需显式指明模板类的模板参数。标准库也
+    发现了这一点，提供了一些辅助模板来解决这个问题
+*/
+auto bar3 = std::make_pair(1,2.9);
 /*
     make_pair是一个模板函数，可以自动推导出函数的两个参数类型。
     从而构造出最终的序对类型，对程序员来说，每写一个模板类都需要考虑封装一个辅助函数来构造
@@ -13,7 +32,8 @@
     在C++17中，std::pair{1,2.9}与std::pair<int,double>{1,2.9}等价。
     通过改写前面的代码可以看到明显的区别，改写后的代码更精简。
 */
-
+std::vector foo3{1,2,3,4};
+std::pair bar4{1,2.9};
 
 template<typename T,typename U>
 struct Pair{
@@ -21,7 +41,7 @@ struct Pair{
     Pair(T,U);
     // ...
 };
-
+Pair foo5{1,2};//编译器能自动推导出Pair<int,int>
 
 // template<typename T,typename U>
 // struct Pair2{
@@ -58,28 +78,5 @@ Pair3(T,U) -> Pair3<T,U>;
 
 int main()
 {
-    //C++17新特性 模板类型推导
-    //Class Template Argument Deduction，缩写为CTAD
-    /*
-        进一步减少冗余代码的重要特性。当我们使用标准库里提供的模板函数时，
-        或许不知道它就是个模板函数，但使用起来就和普通函数一样自然。
-    */
-
-    int foo = std::max(1,2); // #1,省略模板参数的指明,因为两个参数类型一致。
-    double bar = std::max<double>(1, 2.9); // #2,指明模板参数，因为两个参数类型不一致，编译器不知道该使用哪一个类型。
-    /*
-        然而在我们使用模板类时，都必须指明模板类的模板参数后才能使用。
-    */
-    std::vector<int> foo2{1,2,3,4};
-    std::pair<int, double> bar2{1, 2.0};
-    /*
-    初始化一个序对类型时，我们明显可以通过构造传递的两个值的类型得到pair<int，double>,
-    但是在C++17之前，尽管编译器知道这些类型，仍需显式指明模板类的模板参数。标准库也
-    发现了这一点，提供了一些辅助模板来解决这个问题
-    */
-    auto bar3 = std::make_pair(1,2.9);
-    std::vector foo3{1,2,3,4};
-    std::pair bar4{1,2.9};
-    Pair foo5{1,2};//编译器能自动推导出Pair<int,int>
     return 0;
 }
